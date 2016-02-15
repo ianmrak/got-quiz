3ba17d6865a94ee3aa733b9a38b8f202b3b8f099
Ian Mrak <ianmrak@gmail.com>
2016-02-15 15:45:32 -0600

    static quiz page

---
+++ js/questions.js
@@ -0,0 +1,6 @@
+var joffery = {
+  name: 'Joffery Baratheon',
+  nameQuestion: ['Jeffery Baratheon', 'Josh Baratheon', 'Theon Stark', 'Joffery Stark', 'Joffery Baratheon']
+  firstAppearance: 'Season 1, Episode 1',
+
+}
